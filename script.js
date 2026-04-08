aa9f5c4786fe6c2017ebf638701e5137cc59f747
SpaceXDeez <dylanrossi360@gmail.com>
2026-04-08 15:23:16 -0700

    mom is a bum

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function calcAge(birthYear, birthMonth, birthDay) {
+  const today = new Date();
+  let age = today.getFullYear() - birthYear;
+  const hasBirthdayPassed =
+    today.getMonth() + 1 > birthMonth ||
+    (today.getMonth() + 1 === birthMonth && today.getDate() >= birthDay);
+  if (!hasBirthdayPassed) age--;
+  return age;
+}
+
+document.getElementById('age-dylan').textContent = calcAge(2010, 1, 5);
+document.getElementById('age-kate-taylor').textContent = calcAge(2012, 2, 27);
+document.getElementById('age-dylan-card').textContent = calcAge(2010, 1, 5);
+document.getElementById('age-taylor-card').textContent = calcAge(2012, 2, 27);
+document.getElementById('age-katelyn-card').textContent = calcAge(2012, 2, 27);
+
 document.getElementById('change').addEventListener('click', function(){
   const h = document.getElementById('greeting');
   if(h.textContent.includes('Hello')){
